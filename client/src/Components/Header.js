dbf3a93d9e1ca94f62ae0c11ed98288c762602c2
Dmitriy Shin <dimashin9@gmail.com>
2019-01-21 15:28:15 -0500

    from master

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+import Navigation from "./Navigation";
+import LogoImage from "../Assets/Images/logox2.png";
 
 const Header = props => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderWrapper>
+      <Logo src={LogoImage} alt="Spodralux Logo" />
+      <Navigation />
+    </HeaderWrapper>
   );
 };
+
+/**
+ * 
+ * Styled Components
+ * 
+ */
+const HeaderWrapper = styled.header`
+  height: 80px;
+  background: #10069F;
+`;
+
+const Logo = styled.img`
+  height: 40px;
+`;
 
 export default Header;

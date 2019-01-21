3fe7b7a3a5f6bb191a806eb2d801ea030e24dfec
Anthony Machula <x@anthonymachula.ca>
2019-01-21 15:59:41 -0500

    lala land

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
 import Sidebar from './Components/Sidebar.js';
 
-import Header from "./Components/Header";
+// import Header from "./Components/Header";
 
 const App = props => {
   return (
     <div className="container">
-      <Header />
-<<<<<<< HEAD
+      // <Header />
       <Sidebar />
-=======
->>>>>>> dbf3a93d9e1ca94f62ae0c11ed98288c762602c2
     </div>
   );
 };

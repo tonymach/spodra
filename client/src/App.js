71a8010ac065e8b87ba0003321357b576b29e0b9
Dmitriy Shin <dimashin9@gmail.com>
2019-01-21 17:30:11 -0500

    fixed conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
 
 import Header from "./Components/Header";
-<<<<<<< HEAD
 import Sidebar from "./Components/Sidebar";
 
-const App = props => {
-  return (
-    <div>
-=======
 import image1 from "./images/1.jpg";
 import ImageBoard from "./Components/ImageBoard";
-
 
 const App = props => {
   return (
     <div className="container">
->>>>>>> cb58c2e90e8d53f489ca3afc1815fe5aa9361748
       <Header />
       <Sidebar />
       <ImageBoard image={image1} />
